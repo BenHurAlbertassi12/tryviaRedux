9fadab9bc44f26039edce131a01ab778cf74f51e
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-03 11:59:42 -0300

    Login Style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-max-depth */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { MdBuild } from 'react-icons/md';
-import { Center, Input, Button, ButtonGroup } from '@chakra-ui/react';
+import {
+  Center,
+  Input, Button,
+  ButtonGroup,
+  // Container,
+  // AbsoluteCenter,
+  Card,
+  Box,
+} from '@chakra-ui/react';
 import { typeLogin } from '../redux/action/index';
 
 class Login extends Component {
@@ -58,57 +68,70 @@ class Login extends Component {
   render() {
     const { name, email, isBtnDisabled } = this.state;
     return (
-      <Center margin="15px">
-        <form>
-          <Center>
+      <Center h="100vh">
+        <Box
+          bg="#DDDDDD"
+          color="white"
+          p={ 4 }
+          w="50%"
+          borderRadius="md"
+          boxShadow="lg"
+        >
+          <form>
+            <Center>
 
-            <Input
-              marginBottom="15px"
-              data-testid="input-player-name"
-              type="text"
-              value={ name }
-              name="name"
-              placeholder="Digite seu Nome"
-              onChange={ this.handleInput }
-            />
-          </Center>
-          <Center>
-            <Input
-              marginBottom="15px"
-              data-testid="input-gravatar-email"
-              placeholder="Digite seu Email"
-              type="text"
-              value={ email }
-              name="email"
-              onChange={ this.handleInput }
-              required
-            />
-          </Center>
-          <Center>
-            <ButtonGroup direction="row" spacing={ 4 }>
-              <Button
-                data-testid="btn-play"
-                type="submit"
-                disabled={ isBtnDisabled }
-                onClick={ this.handleClick }
-                colorScheme="teal"
-                variant="solid"
-              >
-                Play
-              </Button>
-              <Button
-                leftIcon={ <MdBuild /> }
-                colorScheme="pink"
-                variant="solid"
-                data-testid="btn-settings"
-                type="button"
-                onClick={ this.handleClickSettings }
-              >
-                Settings
-              </Button>
-            </ButtonGroup>
-          </Center>
-        </form>
+              <Input
+                color="black"
+                variant="filled"
+                marginBottom="15px"
+                data-testid="input-player-name"
+                type="text"
+                value={ name }
+                name="name"
+                placeholder="Digite seu Nome"
+                onChange={ this.handleInput }
+              />
+            </Center>
+            <Center>
+              <Input
+                color="black"
+                variant="filled"
+                marginBottom="15px"
+                data-testid="input-gravatar-email"
+                placeholder="Digite seu Email"
+                type="text"
+                value={ email }
+                name="email"
+                onChange={ this.handleInput }
+                required
+              />
+            </Center>
+            <Center>
+              <ButtonGroup direction="row" spacing={ 4 }>
+                <Button
+                  data-testid="btn-play"
+                  type="submit"
+                  disabled={ isBtnDisabled }
+                  onClick={ this.handleClick }
+                  colorScheme="teal"
+                  variant="solid"
+                >
+                  Play
+                </Button>
+                <Button
+                  leftIcon={ <MdBuild /> }
+                  colorScheme="pink"
+                  variant="solid"
+                  data-testid="btn-settings"
+                  type="button"
+                  onClick={ this.handleClickSettings }
+                >
+                  Settings
+                </Button>
+              </ButtonGroup>
+            </Center>
+          </form>
+        </Box>
       </Center>
     );
   }
